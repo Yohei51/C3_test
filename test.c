c257bc0aa623685b103858eb5f90843c6c02a781
yohei <oma05018789@icloud.com>
2024-11-19 15:59:53 +0900

    [add] test

---
+++ test.c
@@ -0,0 +1,28 @@
+/*
+ * C言語のサンプルプログラム - Webkaru
+ * - 長方形の面積を計算 -
+ */
+#include <stdio.h>
+ 
+int main(void)
+{
+
+  /* 長方形の面積 */
+  float area;
+
+  /* よこ・たての長さ */
+  float length_yoko;
+  float length_tate;
+
+  /* よこ・たての長さを入力 */
+  printf("長方形のよこの長さを入力: a = ");
+  scanf("%f", &length_yoko);
+  printf("長方形のたての長さを入力: b = ");
+  scanf("%f", &length_tate);
+
+  /* 長方形の面積を計算・出力 */
+  area = length_yoko * length_tate;
+  printf("長方形の面積: S = %.3f\n", area);
+
+  return 0;
+}
